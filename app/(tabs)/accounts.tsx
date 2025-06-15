896a0ae78d766765f44488c282c7c12b0e36d2be
Eric Minassian <minassianeric2848@gmail.com>
2025-06-14 18:14:16 -0700

    feat: scaffold tabs

---
+++ app/(tabs)/accounts.tsx
@@ -0,0 +1,20 @@
+import { ThemedText } from "@/components/ThemedText";
+import { ThemedView } from "@/components/ThemedView";
+import { StyleSheet } from "react-native";
+
+export default function AccountsScreen() {
+  return (
+    <ThemedView style={styles.content} safe>
+      <ThemedText type="title">Accounts</ThemedText>
+    </ThemedView>
+  );
+}
+
+const styles = StyleSheet.create({
+  content: {
+    flex: 1,
+    padding: 32,
+    gap: 16,
+    overflow: "hidden",
+  },
+});
